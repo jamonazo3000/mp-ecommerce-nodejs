dc1b3a3bb83505c85cb7fb17491949dadf19cf08
NicolasGL <nico17.ngl@gmail.com>
2020-06-24 04:16:15 -0500

    Update MP keys

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-var exphbs  = require('express-handlebars');
- 
+var exphbs = require('express-handlebars');
+
+var mercadopago = require('mercadopago');
+mercadopago.configure({
+    access_token: 'APP_USR-6718728269189792-112017-dc8b338195215145a4ec035fdde5cedf-491494389'
+});
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -12,13 +17,38 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    item = req.query;
+    console.log(item);
+    let preference = {
+        items: [
+            {
+                id: 1234,
+                title: item.title,
+                description: 'Dispositivo móvil de Tienda e-commerce',
+                picture_url: item.img,
+                unit_price: parseInt(item.price),
+                quantity: 1,
+                external_reference: 'nico17.ngl@gmail.com'
+            }
+        ],
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function (response) {
+            global.init_point = response.body.init_point;
+            console.log('init point: ' + global.init_point);
+            console.log('preference id: ' + response.body.id);
+            req.query.id = response.body.id;
+            res.render('detail', req.query);
+        }).catch(function (error) {
+            console.log(error);
+        });
 });
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
- 
+
 app.listen(port, function () {
     console.log('Example app listening on port !');
-   });
+});
